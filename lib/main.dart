6e366b4bca39ff0a5bf1829306e3df83650907ab
Jxhan11 <georgeeapen512@gmail.com>
2023-03-05 22:15:06 +0530

    Emergency cards

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,11 @@ class MyApp extends StatelessWidget {
           color: Color(0xFFEEEEE),
           titleTextStyle: TextStyle(color: Color(0xFF242424),fontSize: 30,letterSpacing: 2),
         ),
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(fontFamily: "ProductSans",fontWeight:FontWeight.w700 ),
+          bodyMedium: TextStyle(fontFamily: "ProductSans",fontWeight:FontWeight.w500 ),
+          bodySmall: TextStyle(fontFamily: "ProductSans",fontWeight:FontWeight.w100 ),
+        ),
       ),
       // Inner UI of the application
       home: const Homepage(),
@@ -61,7 +66,9 @@ class _HomepageState extends State<Homepage> {
         // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           Ads(),
-          FilledCard()
+          EmergencyCards(),
+          // FilledCard(),
+
         ],
       )
 
@@ -81,21 +88,123 @@ class _AdsState extends State<Ads> {
   Widget build(BuildContext context) {
     double screenHeight=MediaQuery.of(context).size.height;
     double screenWidth=MediaQuery.of(context).size.width;
-    return Container(
-      height: 200,
+    final PageController controller = PageController();
+    return SizedBox(
       width: screenWidth,
-      color: Color(0xFFCACACA),
-      child: Center(
-        child: Text(
-          "Ads",
-          style: TextStyle(
-            fontSize: 40,
-
+      height: 240,
+      child: PageView(
+        controller: controller,
+        children: <Widget>[
+          Container(
+            color: Colors.amber,
+            width: screenWidth,
+            height: 200,
+            child: Center(
+              child: Text("This is Ad section 1 ( Drag and Scroll Bro"),
+            ),
           ),
-        ),
+          Container(
+            color: Color(0xFF1B5B81),
+            width: screenWidth,
+            height: 200,
+            child: Center(
+              child: Text("This is Ad section 2"),
+            ),
+          ),
+          Container(
+            color: Colors.blue,
+            width: screenWidth,
+            height: 200,
+            child: Center(
+              child: Text("This is Ad section 3"),
+            ),
+          ),
+          Container(
+            color: Colors.redAccent,
+            width: screenWidth,
+            height: 200,
+            child: Center(
+              child: Text("This is Ad section 4"),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+class EmergencyCards extends StatefulWidget {
+  const EmergencyCards({Key? key}) : super(key: key);
+
+  @override
+  State<EmergencyCards> createState() => _EmergencyCardsState();
+}
+
+class _EmergencyCardsState extends State<EmergencyCards> {
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight=MediaQuery.of(context).size.height;
+    double screenWidth=MediaQuery.of(context).size.width;
+    return Container(
+      // color: Colors.amber,
+      height: 150,
+      margin: EdgeInsets.all(10),
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(color: Color(0xFFFF8E8E),borderRadius: BorderRadius.circular(15)),
+              width: 110,
+              height: 110,
+              // color: Colors.blue,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    IconButton(onPressed: null, icon: Icon(Icons.ice_skating),iconSize: 30,),
+                    Text("Emergency",style: TextStyle(fontSize: 15),),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),border: Border.all(color: Colors.black54)),
+              width: 110,
+              height: 110,
+              // color: Colors.blue,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    IconButton(onPressed: null, icon: Icon(Icons.local_hospital_outlined),iconSize: 30,),
+                    Text("First Aid",style: TextStyle(fontSize: 15),),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),border: Border.all(color: Colors.black54)),
+              width: 110,
+              height: 110,
+              // color: Colors.blue,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    IconButton(onPressed: null, icon: Icon(Icons.credit_card),iconSize: 30,),
+                    Text("Credentials",style: TextStyle(fontSize: 15),),
+                  ],
+                ),
+              ),
+            ),
+            
+          ],
+        ),
+      ),
+    ) ;
+  }
+}
+
 
 
