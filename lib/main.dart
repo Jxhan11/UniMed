b6f615074745250c3ce69b533e1bf37d4c24efe4
Jxhan11 <georgeeapen512@gmail.com>
2023-03-21 00:14:18 +0530

    new app

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
 
       theme: ThemeData(
         primarySwatch: Colors.red,
+        scaffoldBackgroundColor: Color(0xFFF2F2F2),
         fontFamily: "ProductSans",
         appBarTheme: AppBarTheme(
           color: Color(0xFFEEEEE),
@@ -60,38 +61,23 @@ class _HomepageState extends State<Homepage> {
     double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
         appBar: AppBar(
-          title: Row(
-            children: <Widget>[
-              CircleAvatar(
-                backgroundColor: Color(0xFF1C1C1C),
-                radius: 20,
-                child: Container(
-                  height: 37,
-                  width: 37,
-                  decoration: BoxDecoration(color: Color(0xFFF1F1F1),borderRadius: BorderRadius.circular(100)),
-                  child: Center(child: Text("   U   ",style: TextStyle(color: Colors.black,fontFamily: "PSB",fontSize: 15),)),
-                ),
-              ),
-
-              Expanded(child: Container(),),
-              IconButton(onPressed: (){}, icon: Icon(Icons.search),color: Colors.black,splashRadius: 20,),
-              IconButton(onPressed: (){}, icon: Icon(Icons.account_circle_outlined),color: Colors.black,splashRadius: 20,),
-            ],
-          ),
+          title: Text("Welcome"),
           elevation: 0,
         ),
-        body: ListView(
-          scrollDirection: Axis.vertical,
-          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Ads(),
-            EmergencyCards(),
-            DailyNeeds(),
-            PreviouslyOrdered(),
-            // FreqBought(),
-            // FilledCard(),
+        body: CustomScrollView(
+
+          slivers: <Widget>[
+            SubHeadlines(text: 'Records'),
+            SliverToBoxAdapter(
+              child: RecordCards(),
+            ),
+            EmergencyBoxes(),
+            SubHeadlines(text: 'For you'),
+
+
           ],
-        ));
+        ),
+    );
   }
 }
 
@@ -388,3 +374,223 @@ class _EmergencyCardsState extends State<EmergencyCards> {
     );
   }
 }
+
+///////////////////////////////////////NEW UNIMED APP FUNCTIONS/////////////////////////////////////////////////
+
+class RecordCards extends StatefulWidget {
+  const RecordCards({Key? key}) : super(key: key);
+
+  @override
+  State<RecordCards> createState() => _RecordCardsState();
+}
+
+class _RecordCardsState extends State<RecordCards> {
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      // color: Colors.black12,
+      padding:EdgeInsets.only(left: 16,top: 8) ,
+      height: 140,
+      child: Container(
+        child: ScrollConfiguration(
+          behavior: ScrollBehavior(),
+          child: GlowingOverscrollIndicator(
+            axisDirection: AxisDirection.down,
+            color: Colors.transparent,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InkWell(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF9F9F9),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      width: 216,
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(height: 20,),
+                          Container(padding: EdgeInsets.only(left: 15), width:double.infinity, child: Text("Resting Heart Rate",textAlign: TextAlign.left,style: TextStyle(color: Color(0xFFA00000),fontSize: 17),)),
+                          SizedBox(height: 20,),
+                          Container(padding: EdgeInsets.only(left: 15),width: double.infinity, child: Text("99 Bpm",))
+
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InkWell(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF9F9F9),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      width: 216,
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(height: 20,),
+                          Container(padding: EdgeInsets.only(left: 15), width:double.infinity, child: Text("Resting Heart Rate",textAlign: TextAlign.left,style: TextStyle(color: Color(0xFFA00000),fontSize: 17),)),
+                          SizedBox(height: 20,),
+                          Container(padding: EdgeInsets.only(left: 15),width: double.infinity, child: Text("99 Bpm",))
+
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InkWell(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF9F9F9),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      width: 216,
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(height: 20,),
+                          Container(padding: EdgeInsets.only(left: 15), width:double.infinity, child: Text("Resting Heart Rate",textAlign: TextAlign.left,style: TextStyle(color: Color(0xFFA00000),fontSize: 17),)),
+                          SizedBox(height: 20,),
+                          Container(padding: EdgeInsets.only(left: 15),width: double.infinity, child: Text("99 Bpm",))
+
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SubHeadlines extends StatelessWidget {
+  const SubHeadlines({required this.text});
+ final String text;
+  @override
+  Widget build(BuildContext context) {
+    return  SliverToBoxAdapter(
+      child: Column(
+          children:<Widget>[
+            SizedBox(height: 30,),
+            Container(padding: EdgeInsets.only(left: 30), width:double.infinity , child: Text(text,style: TextStyle(fontSize: 17,letterSpacing: 1.5),)),
+            SizedBox(height: 10,)
+          ]
+      ),
+    ) ;
+  }
+}
+
+
+class EmergencyBoxes extends StatelessWidget {
+  const EmergencyBoxes({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Container(
+        padding: EdgeInsets.only(left: 16,right: 16,top: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  color: Color(0xFFFF8E8E),
+                  borderRadius: BorderRadius.circular(10)),
+              width: 110,
+              height: 120,
+              // color: Colors.blue,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.emergency),
+                      iconSize: 30,
+                      color: Color(0xFFA00000),
+                    ),
+                    Text(
+                      "Emergency",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Color(0xFFA00000),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 8),
+              decoration: BoxDecoration(
+                color: Color(0xFFF9F9F9),
+                  borderRadius: BorderRadius.circular(10),
+                  ),
+              width: 110,
+              height: 120,
+              // color: Colors.blue,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.healing),
+                      iconSize: 30,
+                      color: Colors.black,
+                    ),
+                    Text(
+                      "First Aid",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 8),
+              decoration: BoxDecoration(
+                color: Color(0xFFF9F9F9),
+                  borderRadius: BorderRadius.circular(10),
+                  ),
+              width: 110,
+              height: 120,
+              // color: Colors.blue,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.credit_card),
+                      iconSize: 30,
+                      color: Colors.black,
+                    ),
+                    Text(
+                      "Credentials",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
